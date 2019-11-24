2657280408c4cebdcf4318d5533d836171cf3a39
fuji.nguyen <fuji.nguyen@live.com>
2019-11-24 14:46:16 -0500

    label update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-    title = 'HR IPMS Contacts';
+    title = 'Position Description';
 
     menu: Array<Object> = [
         { name: 'Help', url: '/' },
